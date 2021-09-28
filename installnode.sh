18a8d737c6fe8b5e5ebd22b080fe65c551ef3bf5
Thanos420NoScope <rock.gauthier@outlook.fr>
2021-09-28 11:28:35 -0400

    Update 2.9.2

--- installnode.sh
+++ installnode.sh
@@ -93,14 +93,14 @@ apt-get install -y curl >> $LOG_FILE 2>&1
 
 # --- NODE BINARY SETUP --- #
 
-NODE=https://github.com/kadena-io/chainweb-node/releases/download/2.9/chainweb-2.9.ghc-8.10.5.ubuntu-20.04.1cf40e7.tar.gz
+NODE=https://github.com/kadena-io/chainweb-node/releases/download/2.9.2/chainweb-2.9.2.ghc-8.10.7.ubuntu-20.04.ea56802.tar.gz
 MINER=https://github.com/kadena-io/chainweb-miner/releases/download/v1.0.3/chainweb-miner-1.0.3-ubuntu-18.04.tar.gz
 
 decho 'Downloading Node...'
 mkdir -p /root/kda
 cd /root/kda/
 wget --no-check-certificate $NODE >> $LOG_FILE 2>&1
-tar -xvf chainweb-2.9.ghc-8.10.5.ubuntu-20.04.1cf40e7.tar.gz >> $LOG_FILE 2>&1
+tar -xvf chainweb-2.9.2.ghc-8.10.7.ubuntu-20.04.ea56802.tar.gz >> $LOG_FILE 2>&1
 wget --no-check-certificate $MINER >> $LOG_FILE 2>&1
 tar -xvf chainweb-miner-1.0.3-ubuntu-18.04.tar.gz >> $LOG_FILE 2>&1
 
